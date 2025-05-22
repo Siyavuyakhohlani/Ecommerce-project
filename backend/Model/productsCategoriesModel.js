85726e4a803c35c64ce61204ae7d966cc69ba2c9
Siyavuyakhohlani <siyavuyakhohlani2@gmail.com>
2025-05-22 12:03:45 +0200

    updated product_category naming
    updated

--- backend/Model/productsCategoriesModel.js
+++ backend/Model/productsCategoriesModel.js
@@ -1,17 +1,17 @@
 import { pool } from "../config/config.js"
 
 const getCategories = async () => {
-    let [data, rows] = await pool.query("SELECT * FROM Product_Categories");
+    let [data, rows] = await pool.query("SELECT * FROM product_categories");
     return data;
 };
 
 const getSingleCategory = async (id) => {
-    let [data] = await pool.query("SELECT * FROM Product_Categories WHERE category_id = ?", [id]);
+    let [data] = await pool.query("SELECT * FROM product_categories WHERE category_id = ?", [id]);
     return data;
 };
 
 const insertCategory = async (category_name) => {
-    await pool.query("INSERT INTO Product_Categories (category_name) VALUES (?)", [category_name]);
+    await pool.query("INSERT INTO product_categories (category_name) VALUES (?)", [category_name]);
     return "Category inserted successfully!";
 };
 
@@ -27,13 +27,13 @@ const updateCategory = async (id, data) => {
   
     updateValues.push(id);
   
-    const query = `UPDATE Product_Categories SET ${updateFields.join(", ")} WHERE category_id = ?`;
+    const query = `UPDATE product_categories SET ${updateFields.join(", ")} WHERE category_id = ?`;
     await pool.query(query, updateValues);
     return "Category updated successfully!";
 };
 
 const deleteCategory = async (id) => {
-    await pool.query("DELETE FROM Product_Categories WHERE category_id = ?", [id]);
+    await pool.query("DELETE FROM product_categories WHERE category_id = ?", [id]);
     return "Category successfully deleted";
 };
 
